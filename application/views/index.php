fe1edf61fa47595c56282e5f673f529a76e909de
wil93h <rene_93h@hotmail.com>
2016-10-19 14:16:26 -0600

    Cambio de menu
    se modifivo el css para cambiarle el color, el menú se le cambio todo

--- application/views/index.php
+++ application/views/index.php
@@ -56,18 +56,19 @@
         <header id="navigation" class="navbar-inverse navbar-fixed-top animated-header">
 
         <!-- nuevo menu -->
-		<nav class="collapse navbar-collapse " role="navigation">
+		<nav class=" navbar-collapse">	
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">VILLA EL REFUGIO</a>
+          <a  id="logo" class="navbar-brand" href="#body">
+           <img id="logomenu" src="img/logog.png"></a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse navbar-right">
+        <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
             <li ><a href="#body">Principal</a></li>
             <li class="dropdown">
@@ -75,11 +76,7 @@
               <ul class="dropdown-menu">
                 <li><a href="#">Action</a></li>
                 <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
+                
               </ul>
             </li>
             <li><a href="#portfolio">Proyecto</a></li>
